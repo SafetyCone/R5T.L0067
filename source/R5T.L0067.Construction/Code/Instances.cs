ad9d733260245805fab7354072e74a6b1012cc61
davidcoats <david@getsafetycone.com>
2023-10-16 11:28:48 -0700

    Initial commit.

---
+++ source/R5T.L0067.Construction/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.L0067.Construction
+{
+    public static class Instances
+    {
+		
+    }
+}
---
+++ source/R5T.L0067/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.L0067
+{
+	/// <summary>
+	/// Foundation library. (.NET Standard 2.1)
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
